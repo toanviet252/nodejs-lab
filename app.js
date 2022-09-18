5d7decef99cc8e07a1f0a28ba935aeb55b04de89
Toan Viet <toankvfx17375@funix.edu.vn>
2022-09-18 22:46:31 +0700

    lab4.1

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const http = require("http");
-const routes = require("./routes");
+const express = require("express");
+const app = express();
+app.use("/users", (req, res, next) => {
+  res.send(`<p>The Middleware that handles just /</p>`);
+});
+app.use("/", (req, res, next) => {
+  res.send(`<p>The Middleware that handles just /users</p>`);
+});
 
-const server = http.createServer(routes);
-
-server.listen(3001);
+app.listen(3001);
