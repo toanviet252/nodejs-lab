d961f84c1edbc4f9a74eb508db5fb8656ff5cc30
Toan Viet <toankvfx17375@funix.edu.vn>
2022-09-22 09:48:18 +0700

    lab4.2.1

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,11 @@ const path = require("path");
 router.get("/users", (req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "views", "users.html"));
 });
-router.use("/", (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
+router.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "..", "views", "404.html"));
 });
 
 module.exports = router;
